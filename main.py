3ee42185ebed66e33f6c4bb28b5a965f7f895ec1
Ricardo Alonso <159092677+ricardoalonson@users.noreply.github.com>
2024-04-06 16:05:48 -0600

    main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import data
 import time
 import selector
 
-from selenium import webdriver
-from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
